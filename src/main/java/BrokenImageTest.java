d0d3c3bb37f547341579c696cf121dc749b874e5
Irakli <112602982+IkaMastera@users.noreply.github.com>
2025-02-22 11:04:19 +0400

    update: brokenimagetest class

--- src/main/java/BrokenImageTest.java
+++ src/main/java/BrokenImageTest.java
@@ -1,9 +1,15 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BrokenImageTest {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
-        
+        try{
+            driver.get("https://the-internet.herokuapp.com/broken_images");
+        }catch{
+
+        }
+
     }
 }
