6cc5df4379ee2d12dca0d4c6a9574732d867a9a3
Irakli <112602982+IkaMastera@users.noreply.github.com>
2025-03-24 20:26:41 +0400

    add: context menu test

--- src/main/java/ContextMenuTest.java
+++ src/main/java/ContextMenuTest.java
@@ -1,2 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
 public class ContextMenuTest {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/context_menu");
+        Actions actions = new Actions(driver);
+
+        WebElement hotSpot = driver.findElement(By.id("hot-spot"));
+
+        actions.contextClick(hotSpot).perform();
+
+        System.out.println("Context Menu displayed");
+
+        driver.quit();
+    }
 }
