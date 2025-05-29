eb274d0f96f7cef42169012cb3b4afd905eddff9
Irakli <112602982+IkaMastera@users.noreply.github.com>
2025-05-29 11:58:44 +0400

    add: basic setup for dissapering elements test class

--- src/main/java/DisappearingElementsTest.java
+++ src/main/java/DisappearingElementsTest.java
@@ -1,2 +1,6 @@
 public class DisappearingElementsTest {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe")
+        WebDriver driver = new ChromeDriver();
+    }
 }
