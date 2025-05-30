4cbf69ec4d6ee785003a963f65ebfca26d9053eb
Irakli <112602982+IkaMastera@users.noreply.github.com>
2025-05-30 11:04:42 +0400

    add: imports

--- src/main/java/DisappearingElementsTest.java
+++ src/main/java/DisappearingElementsTest.java
@@ -1,3 +1,13 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class DisappearingElementsTest {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe")
