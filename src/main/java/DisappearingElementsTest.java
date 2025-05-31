ec777530480439fb7094327c7c7f6da16840ad63
Irakli <112602982+IkaMastera@users.noreply.github.com>
2025-05-31 13:58:50 +0400

    add

--- src/main/java/DisappearingElementsTest.java
+++ src/main/java/DisappearingElementsTest.java
@@ -12,5 +12,7 @@ public class DisappearingElementsTest {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe")
         WebDriver driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 }
